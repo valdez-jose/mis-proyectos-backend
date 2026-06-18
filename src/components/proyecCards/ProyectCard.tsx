ac5ea20dda3a13728367ad3cad94d0956a25d821
valdez-jose <tu-email-de-github@example.comclear>
2026-06-18 14:05:34 -0300

    Agregamos notas de sugerencia a cada cards

--- src/components/proyecCards/ProyectCard.tsx
+++ src/components/proyecCards/ProyectCard.tsx
@@ -7,57 +7,81 @@ const ProyectCard = () => {
         <h2 className={styles["titulo-principal"]}>Mis proyectos Backend</h2>
       </div>
 
-      {/* Proyectos */}
+      {/* Contenedor de las Cards */}
       <div className={styles.contenedorProyectos}>
-        {/* Proyecto: Turismo en San Luis */}
+        
+        {/* Proyecto: Animales silvestres */}
         <div className={styles.cardProyecto}>
-          <h3>Proyecto: Animales Silvestre</h3>
-          <p>
-            Catálogo digital interactivo y API REST para registrar, monitorear y 
-            conservar información clave sobre especies de nuestra fauna silvestre.
-          </p>
-          <div className={styles["contenedor-enlace"]}>
-            <a href="https://github.com/valdez-jose/fastapi-rest-animales-silvestre" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
-              Ver en GitHub
-            </a>
-             <a href="https://fastapi-rest-animales-silvestre.onrender.com/docs" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
-              Ver API(Swagger)
-            </a>
+          <div className={styles.infoProyecto}>
+            <h3>Proyecto: Animales Silvestres</h3>
+            <p>
+              Catálogo digital interactivo y API REST para registrar, monitorear y 
+              conservar información clave sobre especies de nuestra fauna silvestre.
+            </p>
+          </div>
+          
+          <div className={styles.footerCard}>
+            <div className={styles["contenedor-enlace"]}>
+              <a href="https://github.com/valdez-jose/fastapi-rest-animales-silvestre" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
+                Ver en GitHub
+              </a>
+              <a href="https://fastapi-rest-animales-silvestre.onrender.com/docs" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
+                Ver API(Swagger)
+              </a>
+            </div>
+            <p className={styles.notaRender}>
+              <strong>Nota:</strong> La API está alojada en Render. El primer acceso puede tardar unos segundos mientras el servidor se inicia.
+            </p>
           </div>
         </div> 
 
-        {/* Proyecto: Vevero */}
+        {/* Proyecto: Plantas silvestres */}
         <div className={styles.cardProyecto}>
-          <h3>Proyecto: Plantas Silvestres</h3>
-          <p>
-           Una enciclopedia verde interactiva. Ideal para amantes de la naturaleza que quieren registrar plantas silvestres, 
-           aprender sobre el medio ambiente y cuidar la vegetación local.
-          </p>
-          <div className={styles["contenedor-enlace"]}>
-            <a href="https://github.com/valdez-jose/fastapi-apirest-plantas-silvestre" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
-              Ver en GitHub
-            </a>
+          <div className={styles.infoProyecto}>
+            <h3>Proyecto: Plantas Silvestres</h3>
+            <p>
+              Una enciclopedia verde interactiva. Ideal para amantes de la naturaleza que quieren registrar plantas silvestres, 
+              aprender sobre el medio ambiente y cuidar la vegetación local.
+            </p>
+          </div>
 
-            <a href="https://fastapi-apirest-plantas-silvestre.onrender.com/docs" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
-              Ver API(Swagger)
-            </a>
+          <div className={styles.footerCard}>
+            <div className={styles["contenedor-enlace"]}>
+              <a href="https://github.com/valdez-jose/fastapi-apirest-plantas-silvestre" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
+                Ver en GitHub
+              </a>
+              <a href="https://fastapi-apirest-plantas-silvestre.onrender.com/docs" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
+                Ver API(Swagger)
+              </a>
+            </div>
+            <p className={styles.notaRender}>
+              <strong>Nota:</strong> La API está alojada en Render. El primer acceso puede tardar unos segundos mientras el servidor se inicia.
+            </p>
           </div>
         </div>
 
-        {/* Proyecto: Bicicleteria */}
+        {/* Proyecto: Lista de compras */}
         <div className={styles.cardProyecto}>
-          <h3>Proyecto: Lista de Compras</h3>
-          <p>
-            API REST para el control de gastos en el supermercado. Permite gestionar listas de compras, calcular el total de los productos y
-             ayudarte a mantenerte dentro de tu presupuesto antes de llegar a la caja.
-          </p>
-          <div className={styles["contenedor-enlace"]}>
-            <a href="https://github.com/valdez-jose/express-ts-lista-compra-api" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
-              Ver en GitHub
-            </a>
-            <a href="https://express-ts-lista-compra-api.onrender.com/" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
-              Ver API(Swagger)
-            </a>
+          <div className={styles.infoProyecto}>
+            <h3>Proyecto: Lista de Compras</h3>
+            <p>
+              API REST para el control de gastos en el supermercado. Permite gestionar listas de compras, calcular el total de los productos y
+              ayudarte a mantenerte dentro de tu presupuesto antes de llegar a la caja.
+            </p>
+          </div>
+
+          <div className={styles.footerCard}>
+            <div className={styles["contenedor-enlace"]}>
+              <a href="https://github.com/valdez-jose/express-ts-lista-compra-api" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
+                Ver en GitHub
+              </a>
+              <a href="https://express-ts-lista-compra-api.onrender.com/" target="_blank" rel="noreferrer" className={styles["enlace-proyecto"]}>
+                Ver API(Swagger)
+              </a>
+            </div>
+            <p className={styles.notaRender}>
+              <strong>Nota:</strong> La API está alojada en Render. El primer acceso puede tardar unos segundos mientras el servidor se inicia.
+            </p>
           </div>
         </div>
 
@@ -66,4 +90,4 @@ const ProyectCard = () => {
   )
 }
 
-export default ProyectCard
+export default ProyectCard;
